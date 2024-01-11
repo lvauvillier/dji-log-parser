96505f58789e3721634a33606a63e8b0603e788c
Luc Vauvillier <luc.vauvillier@gmail.com>
2024-01-11 18:12:15 +0100

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,57 @@
 //!
 //! This module provides functionality for parsing DJI log files.
 //!
-//! Binary structure of log files:
+//! ## Encryption in Version 13 and Later
+//! Starting with version 13, log records are AES encrypted and require a specific keychain
+//! for decryption. This keychain must be obtained from DJI using their API. An apiKey is
+//! necessary to access the DJI API.
+//!
+//! ### Obtaining an ApiKey
+//! To acquire an apiKey, follow these steps:
+//! 1. Visit [DJI Developer Technologies](https://developer.dji.com/user) and log in.
+//! 2. Click `CREATE APP`, choose `Open API` as the App Type, and provide the necessary
+//!    details like `App Name`, `Category`, and `Description`.
+//! 3. After creating the app, activate it through the link sent to your email.
+//! 4. On your developer user page, find your app's details to retrieve the ApiKey
+//!    (labeled as the SDK key).
+//!
+//! ## Usage
+//!
+//! ### Initialization
+//! Initialize a `DJILog` instance from a byte slice to access version information and metadata:
+//! ```
+//! let parser = DJILog::from_bytes(&bytes).unwrap();
+//! println!("Version: {:?}", parser.version);
+//! println!("Info: {:?}", parser.info);
+//! ```
+//!
+//! ### Accessing Records
+//! Decrypt records based on the log file version.
+//!
+//! For versions prior to 13:
+//! ```rust
+//! let records = parser.records(DecryptMethod::None);
+//! ```
+//!
+//! For version 13 and later:
+//! ```rust
+//! let records = parser.records(DecryptMethod::ApiKey("__DJI_API_KEY__"));
+//! ```
+//!
+//!
+//! ### Advanced: Manual Keychain Retrieval
+//! For scenarios like caching, offline use, or custom server communication, the library
+//! exposes the internal keychain retrieval process:
+//! ```rust
+//! let keychain_request = parser.keychain_request().unwrap();
+//! let keychains = keychain_request.fetch("__DJI_API_KEY__").unwrap();
+//! let records = parser.records(DecryptMethod::Keychains(keychains));
+//! ```
+//!
+//! Note: Replace `__DJI_API_KEY__` with your actual apiKey.
+//!
+//!
+//! ## Binary structure of log files:
 //!
 //! v4 -> v6
 //! ```text
