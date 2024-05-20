cfa6a146ab0998c595f28d8bd4be5d5cee5f309c
Luc Vauvillier <luc.vauvillier@gmail.com>
2024-05-20 11:42:53 +0200

    Fix Utf-8 decoding errors

--- dji-log-parser/src/record/app_tip.rs
+++ dji-log-parser/src/record/app_tip.rs
@@ -6,6 +6,6 @@ use serde::Serialize;
 #[serde(rename_all = "camelCase")]
 #[br(little, import { length: u16 })]
 pub struct AppTip {
-    #[br(count=length, try_map = String::from_utf8)]
+    #[br(count=length, map = |s: Vec<u8>| String::from_utf8_lossy(&s).trim_end_matches('\0').to_string())]
     pub tip: String,
 }
