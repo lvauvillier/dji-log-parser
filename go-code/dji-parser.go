1954e74b5e7711750b044fedd303fdc2c8befb31
edancain <edan.cain@gmail.com>
2024-10-13 21:59:51 +1300

    Changes to files

--- go-code/dji-parser.go
+++ go-code/dji-parser.go
@@ -21,97 +21,97 @@ import (
 )
 
 func main() {
-    if len(os.Args) < 3 {
-        fmt.Println("Usage error (two arguments only): go run dji-parser.go <input_file> <api_key>")
-        os.Exit(1)
-    }
+	if len(os.Args) < 3 {
+		fmt.Println("Usage error (two arguments only): go run dji-parser.go <input_file> <api_key>")
+		os.Exit(1)
+	}
 
-    inputFile := filepath.Join(".", os.Args[1])
-    apiKey := os.Args[2]
+	inputFile := filepath.Join(".", os.Args[1])
+	apiKey := "3519165b8d4ab74ca7033a64313e6b5" //os.Args[2]
 
-    fileInfo, err := os.Stat(inputFile)
-    if os.IsNotExist(err) {
-        fmt.Printf("Error: The file %s does not exist.\n", inputFile)
-        os.Exit(1)
-    }
+	fileInfo, err := os.Stat(inputFile)
+	if os.IsNotExist(err) {
+		fmt.Printf("Error: The file %s does not exist.\n", inputFile)
+		os.Exit(1)
+	}
 
-    if fileInfo.Size() == 0 {
-        fmt.Printf("Error: The file %s is empty.\n", inputFile)
-        os.Exit(1)
-    }
+	if fileInfo.Size() == 0 {
+		fmt.Printf("Error: The file %s is empty.\n", inputFile)
+		os.Exit(1)
+	}
 
-    fmt.Printf("Input file: %s (Size: %d bytes)\n", inputFile, fileInfo.Size())
+	fmt.Printf("Input file: %s (Size: %d bytes)\n", inputFile, fileInfo.Size())
 
-    file, err := os.Open(inputFile)
-    if err != nil {
-        fmt.Printf("Error opening file: %s\n", err)
-        os.Exit(1)
-    }
-    defer file.Close()
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %s\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-    reader := bufio.NewReader(file)
-    processReader(reader, apiKey)
+	reader := bufio.NewReader(file)
+	processReader(reader, apiKey)
 }
 
 func processReader(reader io.Reader, apiKey string) (*geom.Geometry, error) {
-    data, err := io.ReadAll(reader)
-    if err != nil {
-        return nil, fmt.Errorf("error reading data: %s", err)
-    }
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("error reading data: %s", err)
+	}
 
-    cData := C.CBytes(data)
-    defer C.free(unsafe.Pointer(cData))
-    cLength := C.size_t(len(data))
-    cApiKey := C.CString(apiKey)
-    defer C.free(unsafe.Pointer(cApiKey))
+	cData := C.CBytes(data)
+	defer C.free(unsafe.Pointer(cData))
+	cLength := C.size_t(len(data))
+	cApiKey := C.CString(apiKey)
+	defer C.free(unsafe.Pointer(cApiKey))
 
-    geojsonPtr := C.get_geojson_string_from_bytes((*C.uchar)(unsafe.Pointer(cData)), cLength, cApiKey)
-    if geojsonPtr == nil {
-        errPtr := C.get_last_error()
-        errStr := C.GoString(errPtr)
-        C.c_api_free_string(errPtr)
-        return nil, fmt.Errorf("failed to get GeoJSON: %s", errStr)
-    }
-    defer C.c_api_free_string(geojsonPtr)
+	geojsonPtr := C.get_geojson_string_from_bytes((*C.uchar)(unsafe.Pointer(cData)), cLength, cApiKey)
+	if geojsonPtr == nil {
+		errPtr := C.get_last_error()
+		errStr := C.GoString(errPtr)
+		C.c_api_free_string(errPtr)
+		return nil, fmt.Errorf("failed to get GeoJSON: %s", errStr)
+	}
+	defer C.c_api_free_string(geojsonPtr)
 
-    geojsonStr := C.GoString(geojsonPtr)
-    fmt.Println(geojsonStr)
+	geojsonStr := C.GoString(geojsonPtr)
+	fmt.Println(geojsonStr)
 
-    var fc geom.GeoJSONFeatureCollection
-    err = json.Unmarshal([]byte(geojsonStr), &fc)
-    if err != nil {
-        return nil, fmt.Errorf("error parsing GeoJSON: %s", err)
-    }
+	var fc geom.GeoJSONFeatureCollection
+	err = json.Unmarshal([]byte(geojsonStr), &fc)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing GeoJSON: %s", err)
+	}
 
 	geometry, err := createGeometryFromFeatureCollection(fc)
-    if err != nil {
-        return &geom.Geometry{}, fmt.Errorf("error creating geometry from geoJson: %s", err)
-    }
+	if err != nil {
+		return &geom.Geometry{}, fmt.Errorf("error creating geometry from geoJson: %s", err)
+	}
 
-    return geometry, nil
+	return geometry, nil
 }
 
 func createGeometryFromFeatureCollection(fc geom.GeoJSONFeatureCollection) (*geom.Geometry, error) {
-    var coords []float64
+	var coords []float64
 
-    for _, feature := range fc {
-        if feature.Geometry.IsEmpty() {
-            continue
-        }
+	for _, feature := range fc {
+		if feature.Geometry.IsEmpty() {
+			continue
+		}
 
-        if feature.Geometry.Type() == geom.TypePoint {
-            point := feature.Geometry.MustAsPoint()
-            xy, _ := point.XY()
-            coords = append(coords, xy.X, xy.Y)
-        }
-    }
+		if feature.Geometry.Type() == geom.TypePoint {
+			point := feature.Geometry.MustAsPoint()
+			xy, _ := point.XY()
+			coords = append(coords, xy.X, xy.Y)
+		}
+	}
 
-    if len(coords) < 4 {
-        return nil, fmt.Errorf("not enough valid points to create a linestring")
-    }
+	if len(coords) < 4 {
+		return nil, fmt.Errorf("not enough valid points to create a linestring")
+	}
 
-    seq := geom.NewSequence(coords, geom.DimXY)
-    linestring := geom.NewLineString(seq)
-    geometry := linestring.AsGeometry()
-    return &geometry, nil
+	seq := geom.NewSequence(coords, geom.DimXY)
+	linestring := geom.NewLineString(seq)
+	geometry := linestring.AsGeometry()
+	return &geometry, nil
 }
